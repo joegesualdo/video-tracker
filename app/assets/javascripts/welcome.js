b546c9b921a7b60ad2858fee2322b347c62f9ee2
Joe Gesualdo <joegesualdo@gmail.com>
2013-07-17 18:12:17 -0400

    Add toggle functionality for 'watched' column of videos model

--- app/assets/javascripts/welcome.js
+++ app/assets/javascripts/welcome.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
   Screencast = Backbone.Model.extend({
+    url : function(){
+      return "/videos/" + this.get('id') + ".json";
+    }
   });
 
   var screencast = new Screencast();
@@ -27,17 +30,37 @@ $(document).ready(function(){
            if (model.get('watched') == true){
               $(view.el).addClass('watched');
            }
-           var img = $('<img/>').attr({ 'src' : model.get('image')})
+           
+           // Can not get the image to render
+           var img = '<img src="' + model.get('image') + '"/>';
+           console.log(img);
            $(view.el).append(img);
+
            $('ul.screencasts').append(view.render());
       });
     }
   });
 
-  //-----
+  //----
+
+  
+  
+  //------
 
   var ScreencastView = Backbone.View.extend({
     tagName : "li",
+    events : {
+      "click" : "toggleWatched"
+    },
+    toggleWatched : function(){
+      if (this.model.get('watched') == true){
+        this.model.set({'watched': false}).save();
+        $(this.el).removeClass('watched');
+      } else {
+        this.model.set({'watched' : true}).save();
+        $(this.el).addClass('watched');
+      }
+    },
     render : function(){
       return $(this.el).text(this.model.get('title'));
     }
