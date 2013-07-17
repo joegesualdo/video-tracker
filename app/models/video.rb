fed2fdf52d6aaf1b96ec4bbc5a1555effc65be00
Joe Gesualdo <joegesualdo@gmail.com>
2013-07-17 11:24:14 -0400

    Create rake task to import peepcode videos

---
+++ app/models/video.rb
@@ -0,0 +1,3 @@
+class Video < ActiveRecord::Base
+  attr_accessible :image, :link, :title, :watched
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VideoTracker::Application.config.secret_token = '48ab42bb708b9461c5ca61f27482252db96038f37bb0f1d29e861959f4ecb0235a2e4d84eb06c41cb9fedb42b055c527a118d14dd760ca202689f4c5e1968fd4'
